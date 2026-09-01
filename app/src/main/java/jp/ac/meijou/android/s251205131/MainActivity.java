8c7812af891f8000676f4ef122495ce761bd92b1
しゅん <syunn20061207@gmail.com>
2026-09-01 14:31:15 +0900

    テキストを保存できるようにした

--- app/src/main/java/jp/ac/meijou/android/s251205131/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205131/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s251205131;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 import android.widget.ImageView;
 
@@ -15,6 +17,7 @@ import jp.ac.meijou.android.s251205131.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,47 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+        });
+
+        prefDataStore = PrefDataStore.getInstance(this);
+
+        binding.save.setOnClickListener(view -> {
+            String text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
         });
 
         // TextView text_view = findViewById(R.id.text_view);
         // text_view.setText(R.string.text);
-        binding.textView.setText(R.string.text);
-        binding.vectorAsset.setImageResource(R.drawable.asset123);
+        // binding.textView.setText(R.string.text);
+        // binding.vectorAsset.setImageResource(R.drawable.asset123);
+
+        binding.change.setOnClickListener(view -> {
+            String text = binding.editTextText.getText().toString();
+            binding.textView.setText(text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+               /* String text = s.toString();
+                binding.textView.setText(text); */
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
 
     }
 }
